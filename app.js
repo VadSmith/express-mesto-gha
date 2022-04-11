8cc60b79af3c985d299f56af2f33f327fabc89b8
Vadim Apishev <oldfatham@gmail.com>
2022-04-11 14:06:04 +0700

    connect to mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 300 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log('Express is on port 3000!');
