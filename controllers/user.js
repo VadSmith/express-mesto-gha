2146e76e0ecd7af84aa8638895e14ed9d3e9ba47
Vadim Apishev <oldfatham@gmail.com>
2022-04-12 22:55:22 +0700

    fix response

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,7 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUser = (req, res) => {
-  User.findById({ _id: req.params.userId })
+  User.findById(req.params.userId)
     .then((user) => res.send({ user }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
