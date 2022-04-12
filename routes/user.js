20f0a26caaeb83cbcbe151a178f1713a944a7c46
Vadim Apishev <oldfatham@gmail.com>
2022-04-12 22:11:03 +0700

    add user route

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 const { getUsers } = require('../controllers/user');
 
 router.get('/users', getUsers);
-// router.get('/users', (req, res) => {
-//   console.log(req.baseUrl);
-// });
+
+module.exports = router;
