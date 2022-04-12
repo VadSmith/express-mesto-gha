34fb3b1257d239d42936371b35a2fd5c221ead80
Vadim Apishev <oldfatham@gmail.com>
2022-04-12 22:51:28 +0700

    fix response object

--- controllers/user.js
+++ controllers/user.js
@@ -2,12 +2,12 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.send({ users }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById({ _id: req.params.userId })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({ user }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
