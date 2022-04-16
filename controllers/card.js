78522a22e83e86b1de2a35b503af0a7c1cdf7e27
Vadim Apishev <oldfatham@gmail.com>
2022-04-17 00:12:12 +0700

    fix error handler

--- controllers/card.js
+++ controllers/card.js
@@ -27,7 +27,7 @@ const createCard = (req, res) => {
 const deleteCard = (req, res) => {
   Card.deleteOne({ "_id": req.params.cardId })
     .then(res.send({ message: 'Карточка удалена' }))
-    .catch((err) => console.log(err.name))
+    .catch((err) => next(err));
 
   // .catch((err) => res.status(500).send({ err.message }))
 }
