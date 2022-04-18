6d224729298bb59e2566a6b235924522a8a1199d
Vadim Apishev <oldfatham@gmail.com>
2022-04-18 14:54:17 +0700

    remove morgan lines

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const { PORT = 3000, BASE_URL = 'http://localhost:3000' } = process.env;
@@ -14,7 +12,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   // useFindAndModify: false,
 });
 
-app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
