69845a3de1ab40c73315ed00bc48987144e06360
Vadim Apishev <oldfatham@gmail.com>
2022-04-25 14:40:58 +0700

    improve createUser controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 /* eslint-disable consistent-return */
 const CastError = require('../errors/CastError');
 const NotFoundError = require('../errors/NotFoundError');
@@ -93,17 +94,23 @@ const getUser = (req, res, next) => {
 
 // Создание юзера
 const createUser = (req, res, next) => {
-  const { name, about, avatar } = req.body;
-  User.create(
-    { name, about, avatar },
-  ).then((user) => {
-    res.send(user);
-  }).catch((err) => {
-    if (err.name === 'ValidationError') {
-      return next(new ValidationError('Ошибка: Введены некорректные данные!'));
-    }
-    next(err);
-  });
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create(
+      {
+        name, about, avatar, email, password: hash,
+      },
+    ))
+    .then((user) => {
+      res.send(user);
+    }).catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new ValidationError('Ошибка: Введены некорректные данные!'));
+      }
+      next(err);
+    });
 };
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -1,20 +1,37 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
+    required: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    unique: true,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+    },
+    message: 'Введите корректный email',
+  },
+  password: {
     required: true,
   },
 
