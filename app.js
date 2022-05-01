2fe0af349f38d6f968c7d4c7aa6574689ceeb54f
Vadim Apishev <oldfatham@gmail.com>
2022-05-01 20:45:40 +0700

    add helmet

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 // const { celebrate, Joi } = require('celebrate');
 const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/user');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, BASE_URL = 'http://localhost:3000' } = process.env;
 const app = express();
@@ -20,6 +22,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(helmet());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -32,7 +35,7 @@ app.post('/signin', celebrate({
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
@@ -43,21 +46,13 @@ app.use(auth);
 app.use(require('./routes/card'));
 app.use(require('./routes/user'));
 
-// app.patch('/404', (req, res) => {
-//   res.status(404).send({ message: 'Cтраница не найдена' });
-// });
 app.use((req, res) => {
   res.status(404).send({ message: 'Cтраница не найдена' });
 });
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode)
-    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка!' : message });
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
