22be18db182a0f3eb48afa0a83defab428ee0ed7
Vadim Apishev <oldfatham@gmail.com>
2022-05-16 19:08:22 +0700

    cors

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
   'localhost:3000',
+  'http://127.0.0.1:3000',
   '127.0.0.1:3000',
   'http://localhost:3000',
   'https://localhost:3000',
@@ -43,20 +44,20 @@ app.use(helmet());
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
-  // console.log(origin);
+  console.log(origin);
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Origin', '*');
-  // if (allowedCors.includes(origin)) {
-  //   // res.header('Access-Control-Allow-Origin', origin);
-  //   res.header('Access-Control-Allow-Credentials', true);
-  // }
-  // if (method === 'OPTIONS') {
-  //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  //   res.header('Access-Control-Allow-Headers', requestHeaders);
-  //   return res.end();
-  // }
+  // res.header('Access-Control-Allow-Origin', '*');
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   next();
   return null;
 });
