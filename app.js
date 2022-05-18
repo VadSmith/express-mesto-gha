7e871282ddee50069db886d69a2d1947ece012c8
VadSmith <oldfatham@gmail.com>
2022-05-19 01:07:47 +0700

    cors

--- app.js
+++ app.js
@@ -41,21 +41,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 
-const cors = (req, res, next) => {
+app.use((req, res, next) => {
   const { origin } = req.headers;
+  console.log(origin);
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Credentials', true);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-  return next();
-};
-app.use(cors);
+  next();
+  return null;
+});
+
 app.use(requestLogger); // подключаем логгер запросов
 
 app.post('/signin', celebrate({
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,44 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError');
+// const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_SECRET } = require('../controllers/user');
 
 module.exports = (req, res, next) => {
-  if (!req.cookies.jwt) {
-    return next(new UnauthorizedError('Неправильный email или пароль'));
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
-  const token = req.cookies.jwt;
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return next(new UnauthorizedError('Неправильный email или пароль'));
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
-  req.user = payload;
-  return next();
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  return next(); // пропускаем запрос дальше
 };
+
+// module.exports = (req, res, next) => {
+//   if (!req.cookies.jwt) {
+//     return next(new UnauthorizedError('Неправильный email или пароль'));
+//   }
+//   const token = req.cookies.jwt;
+//   console.log('cookie.jwt in auth: ', token);
+//   let payload;
+//   try {
+//     payload = jwt.verify(token, JWT_SECRET);
+//   } catch (err) {
+//     return next(new UnauthorizedError('Неправильный email или пароль'));
+//   }
+//   req.user = payload;
+//   return next();
+// };
