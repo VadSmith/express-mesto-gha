81a7cbe7edd17bb7a52e8835b5f107bbfd9bf9db
Vadim Apishev <oldfatham@gmail.com>
2022-05-19 21:03:07 +0700

    cors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
       .status(401)
-      .send({ message: 'Необходима авторизация' });
+      .send({ message: 'Необходима авторизация!' });
     // return next(new UnauthorizedError('Неправильный email или пароль'));
   }
 
