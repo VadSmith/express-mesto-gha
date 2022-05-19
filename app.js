ebde81864a86f37bb99248f0d5e9bb78654eb0d7
Vadim Apishev <oldfatham@gmail.com>
2022-05-19 10:43:30 +0700

    fix auth

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use(helmet());
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
-  console.log(origin);
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
-// const UnauthorizedError = require('../errors/UnauthorizedError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_SECRET } = require('../controllers/user');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    // return res
+    //   .status(401)
+    //   .send({ message: 'Необходима авторизация' });
+    return next(new UnauthorizedError('Неправильный email или пароль'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -17,9 +18,10 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    // return res
+    //   .status(401)
+    //   .send({ message: 'Необходима авторизация' });
+    return next(new UnauthorizedError('Неправильный email или пароль'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
