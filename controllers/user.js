e99dcb8e1600d14eeaf457219c6fd79a6b8cb18d
VadSmith <oldfatham@gmail.com>
2022-05-20 21:22:19 +0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -24,7 +24,7 @@ const login = (req, res, next) => {
           res.cookie('jwt', token, {
             maxAge: 3600000,
             httpOnly: true,
-            sameSite: 'none,
+            sameSite: 'none',
             secure: true
           });
           res.send({ message: "Успешный вход" });
