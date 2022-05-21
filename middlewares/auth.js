6d36a891ead039f2387f2555b4f1d01c4043c978
Vadim Apishev <oldfatham@gmail.com>
2022-05-21 16:14:22 +0700

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -14,8 +14,8 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return next(err);
-    // return next(new UnauthorizedError('Необходима авторизация'));
+    // return next(err);
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
 
